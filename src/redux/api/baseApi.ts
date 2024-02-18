0bdb7996b83710e8840d9f80c5c87b11be249179
jubayer44 <jubayerahmedshamim44@gmail.com>
2024-02-18 21:09:44 +0600

    update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,8 +4,8 @@ import { RootState } from "../store";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://sports-management-server.vercel.app/api",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://sports-dashboard-server.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
     credentials: "include",
 
     prepareHeaders: (headers, { getState }) => {
